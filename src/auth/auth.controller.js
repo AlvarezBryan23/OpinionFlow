627074cb64081fcd88f579586812360c34883d76
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2025-02-18 01:13:08 -0600

    Se creo el login para los usuarios

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../user/user.model.js";
-import { hash } from "argon2";
+import { hash, verify } from "argon2";
 
 export const register = async(req, res) =>{
     try{
@@ -21,6 +21,44 @@ export const register = async(req, res) =>{
         console.log(err.message)
         return res.status(500).json({
             message: "User resgistration failed",
+            error: err.message
+        })
+    }
+}
+
+export const login = async(req, res) =>{
+    const {email, username, password} = req.body
+    try{
+        const user = await User.findOne({
+            $or: [{email: email}, {username: username}]
+        })
+
+        if(!user){
+            return res.stats(400).json({
+                message: "Credenciales inválidas",
+                error: "Username o email no existe en la base de datos"
+            })
+        }
+
+        const validPassword = await verify(user.password, password)
+
+        if(!validPassword){
+            return res.status(400).json({
+                message: "Credenciales inválidas",
+                error: "Contraseña incorrecta"
+            })
+        }
+
+        return res.status(200).json({
+            message: "Inicio de sesión exitoso",
+            userDetails:{
+                user
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error en inicio de sesión",
             error: err.message
         })
     }
